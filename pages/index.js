9f137d1ef3761bcdf0f3b169c3af4f477a94673e
William Lee <leewp8@gmail.com>
2022-08-08 17:22:44 +0000

    changed title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Product Advocate Position at <a href="https://vercel.com/">Vercel</a>
         </h1>
 
         <p className={styles.description}>
